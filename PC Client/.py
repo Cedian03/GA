07c85a01e0d12e29eddd1180126578bb673593a9
Cedian03 <algotjalgotjohansson@hotmail.com>
2021-10-30 15:37:14 +0200

    del

--- PC Client/.py
+++
@@ -1,4 +0,0 @@
-def f(key: tuple):
-    return key[0]
-
-print(a := f((1, 2)))
--- PC Client/math_functions/lcm.py
+++
@@ -1,7 +0,0 @@
-"""Lowest common multiple
-"""
-
-from gcd import gcd_recursive as gcd
-
-def lcm(a: int, b: int):
-    return (a * b) // gcd(a, b)
